781cb2b24fd5a87c3cd46f1916128ae52f884454
CR72322 <1209542541@qq.com>
2023-10-22 23:36:33 -0400

    update

--- caesar-cipher/src/main.rs
+++ caesar-cipher/src/main.rs
@@ -1,20 +1,35 @@
-/*
-This code defines two functions: encrypt and decrypt.
-The encrypt function takes a plaintext string and a shift value,
-and returns the ciphertext string.
-The decrypt function takes a ciphertext string and a shift value,
-and returns the plaintext string.
-
-*/
-
+use std::env; // Import the env module.
 use caesar_cipher::decrypt;
 use caesar_cipher::encrypt;
 
 fn main() {
-    let plaintext = "the quick brown fox jumps over the lazy dog";
-    let shift = 3;
+    let args: Vec<String> = env::args().collect(); // Collect the command-line arguments.
+
+    if args.len() != 3 {
+        eprintln!("Usage: cargo run <plaintext> <shift>");
+        std::process::exit(1); // Exit with an error code if the number of arguments is incorrect.
+    }
+
+    let plaintext = &args[1]; // Get the plaintext from the command-line arguments.
+
+    // Attempt to parse the shift as a u8. Exit with an error code if it fails.
+    let shift: u8 = match args[2].parse() {
+        Ok(num) => num,
+        Err(_) => {
+            eprintln!("Error: Shift value must be a positive integer");
+            std::process::exit(1);
+        }
+    };
+
+    // Ensure the shift is a positive integer less than 26.
+    if shift >= 26 {
+        eprintln!("Error: Shift value must be a positive integer less than 26");
+        std::process::exit(1);
+    }
+
     let ciphertext = encrypt(plaintext, shift);
     let decrypted_text = decrypt(&ciphertext, shift);
+
     println!("Plaintext: {}", plaintext);
     println!("Ciphertext: {}", ciphertext);
     println!("Decrypted text: {}", decrypted_text);
